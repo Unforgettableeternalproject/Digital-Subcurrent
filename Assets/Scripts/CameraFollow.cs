66baf48445e471826e4127cd026db9bd97321815
Bernie <ptyc4076@gmail.com>
2024-12-13 15:55:48 +0800

    Added some sample assets, a audio mixer and a simple playable level. 12/13

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Digital_Subcurrent
+{
+    //let camera follow target
+    public class Camera : MonoBehaviour
+    {
+        public Transform target;
+        public float lerpSpeed = 1.0f;
+
+        private Vector3 offset;
+
+        private Vector3 targetPos;
+
+        private void Start()
+        {
+            if (target == null) return;
+
+            offset = transform.position - target.position;
+        }
+
+        private void Update()
+        {
+            if (target == null) return;
+
+            targetPos = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+        }
+
+    }
+}
