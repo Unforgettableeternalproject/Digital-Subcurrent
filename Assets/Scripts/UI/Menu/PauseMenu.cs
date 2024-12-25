12c034004491c4feb8603e8e4d8f6f6b8365baa0
Bernie <ptyc4076@gmail.com>
2024-12-25 11:45:19 +0800

    Added Pause menu. 12/25

---
+++ Assets/Scripts/UI/Menu/PauseMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Digital_Subcurrent
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public static bool IsPaused = false;
+
+        public GameObject pauseMenuUI;
+        public SettingMenu settingMenu;
+        public CanvasGroup canvasGroup;
+
+        void Update() 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (IsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.alpha = 0;
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+        }
+
+        public void Pause()
+        {
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.alpha = 1;
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            IsPaused = true;
+        }
+
+        public void OpenSettingMenu()
+        {
+            Resume();
+            settingMenu.OpenSettingMenu();
+        }
+
+        public void ReturnToMain() 
+        { 
+            Resume();
+        }
+
+        public void ReturnToMap()
+        {
+            Resume();
+        }
+    }
+
+}
